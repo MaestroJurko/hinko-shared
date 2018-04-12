2006341d9845ce88a497a47adb0f0b7162c126ea
Matjaz Jurecic <support@matjazjurecic.com>
2018-04-12 15:19:20 +1200

    creating a git repo for topics

---
+++ shared/default.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+: <<'DESCRIPTION'
+Here we write down the description
+DESCRIPTION
+: <<'ARGUMENTS'
+argumen1: we want number,
+argumen2: we want number
+ARGUMENTS
+
+echo 'Starting to run the script...'
+
+# Here you can run any script (bash|node|go|java|python)
+node topics/default.js
+
+echo 'Script ended'
+
+# Exit code 0        Success
+# Exit code 1        General errors, Miscellaneous errors, such as "divide by zero" and other impermissible operations
+# Exit code 2        Misuse of shell builtins (according to Bash documentation)
+exit 0
